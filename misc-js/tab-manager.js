f45f2af0989a4e11aea827e682d9fbf9d4554a87
EF0601 <edwardfang0601@gmail.com>
2024-06-08 14:01:11 -0400

    updated home page and moved weather app

--- misc-js/tab-manager.js
+++ misc-js/tab-manager.js
@@ -9,6 +9,7 @@ let tabs = {
     calculatorTab: document.getElementById('calculatorTab'),
     canvasTab: document.getElementById('canvasTab'),
     searchTab: document.getElementById('searchTab'),
+    weatherTab: document.getElementById('weatherTab'),
 };
 let tabBtn = {
     homeTabBtn: document.getElementById('home_tab'),
@@ -16,42 +17,61 @@ let tabBtn = {
     calculatorTabBtn: document.getElementById('calculator_tab'),
     canvasTabBtn: document.getElementById('canvas_tab'),
     searchTabBtn: document.getElementById('search_tab'),
+    weatherTabBtn: document.getElementById('weather_tab'),
 };
 
-tabBtn.homeTabBtn.addEventListener('click', () => {
-    tabs.homeTab.style.display = 'block';
+function closeAll() {
+    tabs.homeTab.style.display = 'none';
     tabs.calculatorTab.style.display = 'none';
     tabs.canvasTab.style.display = 'none';
     tabs.searchTab.style.display = 'none';
     tabs.timerTab.style.display = 'none';
+    tabs.weatherTab.style.display = 'none';
+}
+
+function openTab(tab) {
+    closeAll();
+    switch (tab) {
+        case 'home':
+            tabs.homeTab.style.display = 'block';
+            break;
+        case 'timer':
+            tabs.timerTab.style.display = 'block';
+            break;
+        case 'calculator':
+            tabs.calculatorTab.style.display = 'block';
+            break;
+        case 'canvas':
+            tabs.canvasTab.style.display = 'block';
+            break;
+        case 'web explorer':
+            tabs.searchTab.style.display = 'block';
+            break;
+        case 'weather':
+            tabs.weatherTab.style.display = 'block';
+            break;
+        default:
+            break;
+    }
+}
+
+tabBtn.homeTabBtn.addEventListener('click', () => {
+    openTab('home');
 });
 tabBtn.calculatorTabBtn.addEventListener('click', () => {
-    tabs.homeTab.style.display = 'none';
-    tabs.calculatorTab.style.display = 'block';
-    tabs.canvasTab.style.display = 'none';
-    tabs.searchTab.style.display = 'none';
-    tabs.timerTab.style.display = 'none';
+    openTab('calculator');
 });
 tabBtn.canvasTabBtn.addEventListener('click', () => {
-    tabs.homeTab.style.display = 'none';
-    tabs.calculatorTab.style.display = 'none';
-    tabs.canvasTab.style.display = 'block';
-    tabs.searchTab.style.display = 'none';
-    tabs.timerTab.style.display = 'none';
+    openTab('canvas');
 });
 tabBtn.searchTabBtn.addEventListener('click', () => {
-    tabs.homeTab.style.display = 'none';
-    tabs.calculatorTab.style.display = 'none';
-    tabs.canvasTab.style.display = 'none';
-    tabs.searchTab.style.display = 'block';
-    tabs.timerTab.style.display = 'none';
+    openTab('web explorer');
 });
 tabBtn.timerTabBtn.addEventListener('click', () => {
-    tabs.homeTab.style.display = 'none';
-    tabs.calculatorTab.style.display = 'none';
-    tabs.canvasTab.style.display = 'none';
-    tabs.searchTab.style.display = 'none';
-    tabs.timerTab.style.display = 'block';
+    openTab('timer');
+});
+tabBtn.weatherTabBtn.addEventListener('click', () => {
+    openTab('weather');
 });
 
 tabBtn.canvasTabBtn.click();
