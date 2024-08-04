75b145e8dac71691cba9a7c80477ac6020731add
EF0601 <edwardfang0601@gmail.com>
2024-08-04 16:34:33 +0800

    Bring apps back again.
    This time, instead an app AND netlify page will be available.
    
    This adds back electron packages.
    
    From now on, the distributable is generated after the final update, before the distributable is taken out of the repo and changes discarded. This happens directly before the pull request.

---
+++ electronSetup.js
@@ -0,0 +1,28 @@
+console.log("Initializing app...");
+
+const { app, BrowserWindow } = require('electron/main');
+
+const createWindow = () => {
+    const win = new BrowserWindow({
+        width: 1920,
+        height: 1080,
+    });
+
+    win.loadFile('index.html');
+};
+
+app.whenReady().then(() => {
+    createWindow();
+
+    app.on('activate', () => {
+        if (BrowserWindow.getAllWindows().length === 0) {
+            createWindow();
+        }
+    });
+});
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
+});
